c131c65375bcce2cf3fdd3755583635b9ed44fb2
Michael Schams <typo3@2018.schams.net>
2018-10-16 13:42:07 +1100

    [TASK] Extend AttachmentService to allow multiple file uploads

--- Classes/Service/AttachmentService.php
+++ Classes/Service/AttachmentService.php
@@ -1,15 +1,18 @@
 <?php
-
 namespace Mittwald\Typo3Forum\Service;
 
+use Mittwald\Typo3Forum\Domain\Model\Forum\Attachment;
+
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 class AttachmentService implements SingletonInterface
 {
-
     /**
-     * An instance of the Extbase object manager.
+     * Instance of the Extbase object manager.
+     *
+     * @access protected
      * @var \TYPO3\CMS\Extbase\Object\ObjectManagerInterface
      * @inject
      */
@@ -17,6 +20,8 @@ class AttachmentService implements SingletonInterface
 
     /**
      * Converts HTML-array to an object
+     *
+     * @access public
      * @param array $attachments
      * @return ObjectStorage
      */
@@ -25,30 +30,37 @@ class AttachmentService implements SingletonInterface
         /* @var \Mittwald\Typo3Forum\Domain\Model\Forum\Attachment */
         $objAttachments = new ObjectStorage();
 
-        foreach ($attachments as $attachmentID => $attachment) {
-            if ($attachment['name'] == '') {
-                continue;
-            }
-            $tmp_name = $_FILES['tx_typo3forum_pi1']['tmp_name']['attachments'][$attachmentID];
-            $mime_type = mime_content_type($tmp_name);
+        foreach ($attachments as $attachment) {
+            foreach ($attachment as $attachmentID => $eachAttachment) {
+                if ($eachAttachment['name'] == '') {
+                    continue;
+                }
+                $tmp_name = $_FILES['tx_typo3forum_pi1']['tmp_name']['attachments'][$attachmentID];
 
-            //Save in ObjectStorage and in file system
-            $attachmentObj = $this->objectManager->get(\Mittwald\Typo3Forum\Domain\Model\Forum\Attachment::class);
-            $attachmentObj->setFilename($attachment['name']);
-            $attachmentObj->setRealFilename(sha1($attachment['name'] . time()));
-            $attachmentObj->setMimeType($mime_type);
+                // Save in ObjectStorage and file system
+                $attachmentObj = $this->objectManager->get(Attachment::class);
+                $attachmentObj->setFilename($eachAttachment['name']);
+                $attachmentObj->setRealFilename(sha1($eachAttachment['name'] . time()));
 
-            //Create dir if not exists
-            $tca = $attachmentObj->getTCAConfig();
-            $path = $tca['columns']['real_filename']['config']['uploadfolder'];
-            if (!file_exists($path)) {
-                mkdir($path, 0777, true);
-            }
+                //$attachmentObj->setMimeType(mime_content_type($tmp_name));
+                $attachmentObj->setMimeType($eachAttachment['type']);
 
-            //upload file and put in object storage
-            $res = \TYPO3\CMS\Core\Utility\GeneralUtility::upload_copy_move($tmp_name, $attachmentObj->getAbsoluteFilename());
-            if ($res === true) {
-                $objAttachments->attach($attachmentObj);
+                // Create directory if it does not exist
+                $tca = $attachmentObj->getTCAConfig();
+                $path = $tca['columns']['real_filename']['config']['uploadfolder'];
+                if (!file_exists($path)) {
+                    // @TODO fix insecure permissions
+                    mkdir($path, 0777, true);
+                }
+
+                // Move uploaded file to final location
+                $res = GeneralUtility::upload_copy_move(
+                    $eachAttachment['tmp_name'],
+                    $attachmentObj->getAbsoluteFilename()
+                );
+                if ($res === true) {
+                    $objAttachments->attach($attachmentObj);
+                }
             }
         }
         return $objAttachments;
