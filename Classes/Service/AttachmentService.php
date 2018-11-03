69bfa817aa04e6996e18db6893351a12196d858f
DavidBruchmann <david.bruchmann@gmail.com>
2018-11-03 19:28:12 +0700

    related to #43 and #46

--- Classes/Service/AttachmentService.php
+++ Classes/Service/AttachmentService.php
@@ -19,50 +19,87 @@ class AttachmentService implements SingletonInterface
     protected $objectManager = null;
 
     /**
-     * Converts HTML-array to an object
+     * Converts array of sent $_FILES to an ObjectStorage wizth object(s) of type
+     * \Mittwald\Typo3Forum\Domain\Model\Forum\Attachment and moves the files
      *
      * @access public
+     *
      * @param array $attachments
-     * @return ObjectStorage
+     *
+     * @return ObjectStorage<\Mittwald\Typo3Forum\Domain\Model\Forum\Attachment>
      */
     public function initAttachments(array $attachments)
     {
-        /* @var \Mittwald\Typo3Forum\Domain\Model\Forum\Attachment */
         $objAttachments = new ObjectStorage();
-
-        foreach ($attachments as $attachment) {
-            foreach ($attachment as $attachmentID => $eachAttachment) {
-                if ($eachAttachment['name'] == '') {
-                    continue;
-                }
-                $tmp_name = $_FILES['tx_typo3forum_pi1']['tmp_name']['attachments'][$attachmentID];
-
-                // Save in ObjectStorage and file system
-                $attachmentObj = $this->objectManager->get(Attachment::class);
-                $attachmentObj->setFilename($eachAttachment['name']);
-                $attachmentObj->setRealFilename(sha1($eachAttachment['name'] . time()));
-
-                //$attachmentObj->setMimeType(mime_content_type($tmp_name));
-                $attachmentObj->setMimeType($eachAttachment['type']);
-
-                // Create directory if it does not exist
-                $tca = $attachmentObj->getTCAConfig();
-                $path = $tca['columns']['real_filename']['config']['uploadfolder'];
-                if (!file_exists($path)) {
-                    // @TODO fix insecure permissions
-                    mkdir($path, 0777, true);
-                }
-
-                // Move uploaded file to final location
-                $res = GeneralUtility::upload_copy_move(
-                    $eachAttachment['tmp_name'],
-                    $attachmentObj->getAbsoluteFilename()
-                );
-                if ($res === true) {
-                    $objAttachments->attach($attachmentObj);
-                }
-            }
+        foreach ($attachments as $singleAttachmentStack) {
+            $this->initSingleAttachmentStack($singleAttachmentStack, $objAttachments);
         }
         return $objAttachments;
+    }
+
+    /**
+     * @see initAttachments(array $attachments)
+     *
+     * @param $singleAttachmentStack
+     * @param $objAttachments \TYPO3\CMS\Extbase\Persistence\ObjectStorage
+     */
+    protected function initSingleAttachmentStack($singleAttachmentStack, $objAttachments)
+    {
+        foreach ($singleAttachmentStack as $attachmentID => $singleAttachment) {
+            $this->initSingleAttachment($singleAttachment, $objAttachments);
+        }
+    }
+
+    /**
+     * Converts single file of $_FILES as \Mittwald\Typo3Forum\Domain\Model\Forum\Attachment,
+     * saves this Attachment in ObjectStorage and moves the file to final location in file system
+     *
+     * @see initAttachments(array $attachments)
+     *
+     * @param $singleAttachmentStack
+     * @param $attachmentID
+     * @param $objAttachments \TYPO3\CMS\Extbase\Persistence\ObjectStorage
+     */
+    protected function initSingleAttachment(array $singleAttachment, $attachmentID, $objAttachments)
+    {
+        $requiredKeys = ['name', 'type', 'tmp_name'];
+        foreach ($requiredKeys as $requiredKey) {
+            if (!array_key_exists($requiredKey, $singleAttachment) || !$singleAttachment[$requiredKey]) {
+                return;
+            }
+        }
+        $tmp_name = $_FILES['tx_typo3forum_pi1']['tmp_name']['attachments'][$attachmentID];
+
+        /* @var \Mittwald\Typo3Forum\Domain\Model\Forum\Attachment */
+        $attachmentObj = $this->objectManager->get(Attachment::class);
+        $attachmentObj->setFilename($singleAttachment['name']);
+        $attachmentObj->setRealFilename(sha1($singleAttachment['name'] . time()));
+
+        //$attachmentObj->setMimeType(mime_content_type($tmp_name));
+        $attachmentObj->setMimeType($singleAttachment['type']);
+
+        // Create directory if it does not exist
+        $tca = $attachmentObj->getTCAConfig();
+        $path = $tca['columns']['real_filename']['config']['uploadfolder'];
+        if (!file_exists($path)) {
+            // @TODO fix permissions on base of setting in LocalConfiguration.php
+            // (or another external setting)
+            mkdir($path, 0664, true);
+        }
+
+        // Move uploaded file to final location in file system
+        $res = GeneralUtility::upload_copy_move(
+            $singleAttachment['tmp_name'],
+            $attachmentObj->getAbsoluteFilename()
+        );
+
+        // @TODO: fix file-permissions on base of settings -> 0664 / 0644
+        // Which settings, LocalConfiguration.php?
+        // NO execution desired in forum!
+
+        if ($res === true) {
+            // Save in ObjectStorage
+            $objAttachments->attach($attachmentObj);
+        }
     }
 }
