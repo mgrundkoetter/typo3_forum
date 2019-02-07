8808dd5612719675041cf2b04f2e0630585bbd34
Michael Grundkötter <m.grundkoetter@googlemail.com>
2019-02-07 12:15:47 +0100

    Solves #15

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,31 +1,31 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'typo3_forum',
-    'description' => 'Forum extension',
-    'category' => 'plugin',
-    'author' => 'Mittwald CM Service',
-    'author_email' => 'support@mittwald.de',
-    'author_company' => 'Mittwald CM Service',
-    'dependencies' => 'cms,extbase,fluid,sr_feuser_register,static_info_tables',
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => 'typo3temp/typo3_forum,typo3temp/typo3_forum/gravatar',
-    'modify_tables' => 'fe_users',
-    'clearCacheOnLoad' => 0,
+    'title'            => 'typo3_forum',
+    'description'      => 'Forum extension',
+    'category'         => 'plugin',
+    'author'           => 'Mittwald CM Service',
+    'author_email'     => 'support@mittwald.de',
+    'author_company'   => 'Mittwald CM Service',
+    'dependencies'     => 'cms,extbase,fluid,sr_feuser_register,static_info_tables',
+    'state'            => 'stable',
+    'uploadfolder'     => true,
+    'createDirs'       => 'typo3temp/typo3_forum,typo3temp/typo3_forum/gravatar,uploads/tx_typo3forum/workflowstatus,uploads/tx_typo3forum/attachements',
+    'modify_tables'    => 'fe_users',
+    'clearCacheOnLoad' => false,
 
     // NOTE: DO NOT CHANGE this version number manually.
     // This is done by the build-release.sh script.
-    'version' => '8.7.1',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '8.7.0-8.7.99',
+    'version'          => '8.7.2',
+    'constraints'      => [
+        'depends'  => [
+            'typo3'              => '8.7.0-8.7.99',
             'static_info_tables' => '',
-            'php' => '',
+            'php'                => '',
         ],
         'suggests' => [
             'sr_feuser_register' => '',
-            'secure_downloads' => '3.0',
+            'secure_downloads'   => '3.0',
         ],
     ],
 ];
